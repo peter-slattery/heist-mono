f8760f0e3572574213820804995fd010c808f7f1
Peter Slattery <slattery.peter.j@gmail.com>
2023-09-02 19:06:44 -0700

    Implementing an authenticated fetch

--- src/auth/AuthContext.tsx
+++ src/auth/AuthContext.tsx
@@ -7,7 +7,8 @@ export type AuthContext = {
   loading: boolean,
   user: User | null,
   openLoginForm: typeof openLoginForm,
-  logout: () => Promise<void>
+  logout: () => Promise<void>,
+  fetch: (input: RequestInfo, init?: RequestInit) => ReturnType<typeof fetch>
 }
 
 const Context = createContextWithoutDefault<AuthContext>()
@@ -15,6 +16,7 @@ const Context = createContextWithoutDefault<AuthContext>()
 export const AuthContextProvider = ({ children }: React.PropsWithChildren) => {
   const [loading, setLoading] = useState(true)
   const [user, setUser] = useState<User | null>(null)
+  const [jwt, setJwt] = useState<string | null>(null)
 
   useEffectOnMount(() => {
     netlifyIdentity.on("init", (user) => {
@@ -24,8 +26,11 @@ export const AuthContextProvider = ({ children }: React.PropsWithChildren) => {
     })
     netlifyIdentity.on('login', (user) => {
       console.log("Log in", user)
-      setUser(user);
-      closeLoginForm();
+      netlifyIdentity.refresh().then((jwt) => {
+        setUser(user);
+        setJwt(jwt)
+        closeLoginForm();
+      })
     });
     netlifyIdentity.init()
   })
@@ -33,6 +38,29 @@ export const AuthContextProvider = ({ children }: React.PropsWithChildren) => {
   const logout = async () => {
     await netlifyIdentity.logout()
     setUser(null)
+    setJwt(null)
+  }
+
+  const authenticatedFetch = (input: RequestInfo, init?: RequestInit) => {
+    if (!user) {
+      throw new Error("Attempting to call authenticatedFetch before the user is logged in")
+    }
+    const Authorization = `Bearer ${jwt}`
+    if (!init) {
+      init = {
+        headers: {
+          Authorization
+        }
+      }
+    }
+    else if (!init.headers) {
+      init.headers = {
+        Authorization
+      }
+    } else {
+      throw new Error("init already had an Authorization header")
+    }
+    return fetch(input, init)
   }
 
   return (
@@ -42,6 +70,7 @@ export const AuthContextProvider = ({ children }: React.PropsWithChildren) => {
         user,
         openLoginForm,
         logout,
+        fetch: authenticatedFetch
       }}
     >
       {children}
