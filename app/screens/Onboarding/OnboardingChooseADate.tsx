a56881c1265babd57d0cda8d10621c1bf06aa683
Peter Slattery <slattery.peter.j@gmail.com>
2023-09-03 12:46:48 -0700

    Fixing type references in OnboardingChooseADate

--- app/screens/Onboarding/OnboardingChooseADate.tsx
+++ app/screens/Onboarding/OnboardingChooseADate.tsx
@@ -1,6 +1,7 @@
 import { H4 } from "../../designSystem/Typography"
 import { createUseStyles, useTheme } from "../../theme"
-import { useOnboarding, DaydreamOptionKey } from "./OnboardingContext"
+import { useOnboarding } from "./OnboardingContext"
+import { DaydreamOptionKey } from "@heist/common/types"
 
 const useStyles = createUseStyles(() => ({
   layout: {
@@ -16,22 +17,24 @@ const useStyles = createUseStyles(() => ({
   buttonRow: {
     marginTop: 15,
     display: "flex",
-    gap: 8
+    gap: 8,
   },
 }))
 
 const daydreamMessages: Record<DaydreamOptionKey, string> = {
-  "wedding": "Great, let’s get you wedding ready.\nWhen are you planning the big day to be?\n\nAn estimate is fine!",
-  "home": "Great, let’s get you ready to find your home.\nWhen are you planning to pull the trigger?\n\nAn estimate is fine!",
-  "travel": "Great, let’s get you travel ready.\nWhen are you planning on leaving?\n\nAn estimate is fine!",
-  "financial_flexibility": "Uh, Cloj, what do I put here?",
+  wedding:
+    "Great, let’s get you wedding ready.\nWhen are you planning the big day to be?\n\nAn estimate is fine!",
+  home: "Great, let’s get you ready to find your home.\nWhen are you planning to pull the trigger?\n\nAn estimate is fine!",
+  travel:
+    "Great, let’s get you travel ready.\nWhen are you planning on leaving?\n\nAn estimate is fine!",
+  financial_flexibility: "Uh, Cloj, what do I put here?",
 }
 
 export const OnboardingChooseADate = () => {
   const theme = useTheme()
-  const styles = useStyles({theme})
+  const styles = useStyles({ theme })
 
-  const { daydreamOption  } = useOnboarding()
+  const { daydreamOption } = useOnboarding()
   if (!daydreamOption) throw new Error("daydreamOption must be set")
 
   const message = daydreamMessages[daydreamOption]
@@ -40,9 +43,7 @@ export const OnboardingChooseADate = () => {
     <div className={styles.layout}>
       <div className={styles.contentBox}>
         <H4>{message}</H4>
-        <div className={styles.buttonRow}>
-          
-        </div>
+        <div className={styles.buttonRow}></div>
       </div>
     </div>
   )
