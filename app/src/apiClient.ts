2cf9752ffad719be06c3d020892d56d527e7cce7
Peter Slattery <slattery.peter.j@gmail.com>
2023-09-04 13:20:07 -0700

    Changing api root

--- app/src/apiClient.ts
+++ app/src/apiClient.ts
@@ -1,33 +1,47 @@
 import { ApiClient } from "@heist/common/contracts"
 import { authenticatedFetch } from "./utils/authenticatedFetch"
 
+const API_ROOT = "/.netlify/functions"
+
 export const makeApiClient = (jwt: string | null) => {
   const result: ApiClient = {
     userProfileGet: async (_req) => {
-      const resp = await authenticatedFetch(jwt, "/api/userProfileGet", {
+      const resp = await authenticatedFetch(jwt, `${API_ROOT}/userProfileGet`, {
         method: "GET",
       })
       return resp.json()
     },
     userProfileCreate: async (_req) => {
-      const resp = await authenticatedFetch(jwt, "/api/userProfilesCreate", {
-        method: "POST",
-      })
+      const resp = await authenticatedFetch(
+        jwt,
+        `${API_ROOT}/userProfilesCreate`,
+        {
+          method: "POST",
+        }
+      )
       return resp.json()
     },
     userPurchasesGet: async (_req) => {
-      const resp = await authenticatedFetch(jwt, "/api/userPurchasesGet", {
-        method: "POST",
-        body: JSON.stringify(_req),
-      })
+      const resp = await authenticatedFetch(
+        jwt,
+        `${API_ROOT}/userPurchasesGet`,
+        {
+          method: "POST",
+          body: JSON.stringify(_req),
+        }
+      )
       console.log("Resp", resp)
       return resp.json()
     },
     userPurchaseCreate: async (_req) => {
-      const resp = await authenticatedFetch(jwt, "/api/userPurchaseCreate", {
-        method: "POST",
-        body: JSON.stringify(_req),
-      })
+      const resp = await authenticatedFetch(
+        jwt,
+        `${API_ROOT}/userPurchaseCreate`,
+        {
+          method: "POST",
+          body: JSON.stringify(_req),
+        }
+      )
       return resp.json()
     },
   }
