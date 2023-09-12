ba50b65fb32c37f1e0436bffad67c688eedd53c7
Peter Slattery <slattery.peter.j@gmail.com>
2023-09-12 16:52:41 -0700

    More vendors in heist plugin

--- plugins/chrome/main.ts
+++ plugins/chrome/main.ts
@@ -43,7 +43,7 @@ document.head.appendChild(stylesheet)
 type Vendor_Spec = {
   vendor: string
   url: string
-  placement: string
+  placement: string[]
   price_id_selectors: string[]
   image_selectors: string[]
   name_selectors: string[]
@@ -53,23 +53,35 @@ const vendor_specs: Vendor_Spec[] = [
   {
     vendor: "Amazon",
     url: "amazon.com",
-    placement: ".add-to-cart-data",
-    price_id_selectors: [".a-price .a-offscreen"],
-    image_selectors: [],
-    name_selectors: [],
+    placement: [".add-to-cart-data", "#partialStateBuybox"],
+    price_id_selectors: [".a-price .a-offscreen", '[data-a-color="price"]'],
+    image_selectors: ['[data-a-image-name="landingImage"]'],
+    name_selectors: ["#productTitle"],
   },
   {
     vendor: "West Elm",
     url: "westelm.com",
-    placement: ".add-to-cart-container > div > .atc-container",
+    placement: [".add-to-cart-container > div > .atc-container"],
     price_id_selectors: [".purchasing-container .product-price .amount"],
     image_selectors: ["#hero-0"],
     name_selectors: [".product-info .heading-title-pip"],
   },
   {
+    vendor: "Anthropologie",
+    url: "anthropologie.com/shop/",
+    placement: [
+      ".c-pwa-product-info-supplemental .u-pwa-form-field.o-pwa-primary-actions",
+    ],
+    price_id_selectors: [
+      ".c-pwa-product-container .c-pwa-product-price__current.s-pwa-product-price__current",
+    ],
+    image_selectors: [".c-pwa-product-container source"],
+    name_selectors: [".c-pwa-product-container h1"],
+  },
+  {
     vendor: "Anthropologie Living",
     url: "anthropologie.com/anthroliving/shop",
-    placement: ".u-pwa-form-field.o-pwa-primary-actions",
+    placement: [".u-pwa-form-field.o-pwa-primary-actions"],
     price_id_selectors: [
       ".c-pwa-product-price__current.s-pwa-product-price__current",
     ],
@@ -79,12 +91,48 @@ const vendor_specs: Vendor_Spec[] = [
   {
     vendor: "Home Depot",
     url: "homedepot.com/p/",
-    placement: ".buybox__actions--atc",
+    placement: [".buybox__actions--atc"],
     price_id_selectors: [
       ".price-format__large.price-format__main-price :nth-child(2)",
     ],
     image_selectors: [".mediagallery__mainimage--clickable img"],
     name_selectors: [".product-details__badge-title--wrapper h1"],
+  },
+  {
+    vendor: "Lowes",
+    url: "lowes.com/pd/",
+    placement: [".atc-buy-box"],
+    price_id_selectors: [".main-price"],
+    image_selectors: ["#imgCont0 img"],
+    name_selectors: ["#stickyNav-container #pdp-lpd h1"],
+  },
+  {
+    vendor: "Target",
+    url: "target.com/p/",
+    placement: [".styles__ThreeUpButtonWrapper-sc-11rka0i-0"],
+    price_id_selectors: ['[data-test="product-price"]'],
+    image_selectors: ['[data-test="image-gallery-item-0"] img'],
+    name_selectors: ['[data-test="product-title"]'],
+  },
+  {
+    vendor: "Wayfair",
+    url: "wayfair.com/",
+    placement: [".CallToActionGrid-addToCartButton"],
+    price_id_selectors: ['[data-enzyme-id="PdpLayout-infoBlock"] .SFPrice'],
+    image_selectors: ['[data-enzyme-id="InitialImage"]'],
+    name_selectors: [
+      '[data-enzyme-id="PdpLayout-infoBlock"] .ProductDetailInfoBlock-header [data-hb-id="Heading"]',
+    ],
+  },
+  {
+    vendor: "Etsy",
+    url: "etsy.com/listing",
+    placement: ['.add-to-cart-form [data-selector="add-to-cart-button"]'],
+    price_id_selectors: [
+      '#listing-page-cart [data-selector="price-only"] > p:first-child',
+    ],
+    image_selectors: ['[data-perf-group="main-product-image"]'],
+    name_selectors: ["h1"],
   },
 ]
 const vendor_specs_matching = vendor_specs.filter((vendor) => {
@@ -114,7 +162,7 @@ function heist_on_invest(event: MouseEvent) {
   )
   const images = queryAllSelectors<HTMLImageElement>(
     vendor_spec.image_selectors
-  ).map((p) => p.src)
+  ).map((p) => p.src || p.srcset.split(" ")[0])
 
   log(prices[0].innerText, names[0], images[0])
   const data: PluginInvestData = {
@@ -128,21 +176,26 @@ function heist_on_invest(event: MouseEvent) {
 }
 
 function populate_from_spec(spec: Vendor_Spec) {
-  const parents = document.querySelectorAll(spec.placement)
-  for (let i = 0; i < parents.length; i++) {
-    const button = parents[i]
-    if (!button) continue
+  for (let j = 0; j < spec.placement.length; j++) {
+    const selector = spec.placement[j]
+    const parents = document.querySelectorAll(selector)
+    for (let i = 0; i < parents.length; i++) {
+      const button = parents[i]
+      if (!button) continue
 
-    const html = `
-<div class="heist_cta">
-  HEIST
-</div>
-`
-    const insertion = document.createElement("div")
-    insertion.classList.add("heist_add_to_cart")
-    insertion.addEventListener("click", heist_on_invest)
-    insertion.innerHTML = html
-    button.appendChild(insertion)
+      const html = `
+  <div class="heist_cta">
+    HEIST
+  </div>
+  `
+      const insertion = document.createElement("div")
+      insertion.classList.add("heist_add_to_cart")
+      insertion.addEventListener("click", heist_on_invest)
+      insertion.innerHTML = html
+      button.appendChild(insertion)
+
+      return
+    }
   }
 }
 
